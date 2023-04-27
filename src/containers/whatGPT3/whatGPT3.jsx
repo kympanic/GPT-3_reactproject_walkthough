2b2cfdeee3b95831fc47b49836ecb5c72f592448
Daniel Yoo <dacapo0509@gmail.com>
2023-04-26 18:00:21 -0700

    added index.js for containers

--- src/containers/whatGPT3/whatGPT3.jsx
+++ src/containers/whatGPT3/whatGPT3.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import "./whatGPT3.css";
 
-const whatGPT3 = () => {
-	return <div>whatGPT3</div>;
+const WhatGPT3 = () => {
+	return <div>WhatGPT3</div>;
 };
 
-export default whatGPT3;
+export default WhatGPT3;
